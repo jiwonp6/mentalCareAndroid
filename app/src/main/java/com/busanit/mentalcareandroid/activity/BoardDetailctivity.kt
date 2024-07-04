5eb12eaf37862327b49bb1716d532f4012434a35
Hwangsj-qk <sujeong7191@naver.com>
2024-07-04 14:33:32 +0900

    initial commit

---
+++ app/src/main/java/com/busanit/mentalcareandroid/activity/BoardDetailctivity.kt
@@ -0,0 +1,21 @@
+package com.busanit.mentalcareandroid.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.busanit.mentalcareandroid.R
+
+class BoardDetailctivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_board_detailctivity)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
